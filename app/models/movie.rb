274d44fefaa74fd22016ea411ec0f2e131d057e0
Trace <tracedelange@me.com>
2021-08-24 13:52:11 -0500

    commit tree f496e95b7a97b42eae5c194032e079d00d641362

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,23 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+
+    def self.first_movie
+        Movie.all[0]
+    end
+
+    def self.last_movie
+        Movie.all[Movie.all.length - 1]
+    end
+
+    def self.movie_count
+        Movie.all.length
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.where(id: id)
+    end
 end
